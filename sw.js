86787ff51bd31b292be97b7daf040cd46987428f
yutthapong-jpg <yutthapong@maximafurniture.com>
2026-08-10 14:14:09 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('draftman-store').then((cache) => cache.addAll([
-      './index.html',
-      './manifest.json'
-    ]))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  // Dummy fetch event to satisfy PWA requirements
 });
